e4be7cf4cd4fd8baf1ebf07eaa7a011c47e1a677
sugarforever <william.1022@gmail.com>
2025-09-17 11:31:58 +0100

    feat: improved experience on mdesktop and mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://blog.chatollama.cloud',
   integrations: [
     mdx(),
     tailwind({
